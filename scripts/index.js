a3fbd22f8993bd7149080799065ba8d5c6819ac7
Habib ur rehman Bhatti <109253977+HabibUrRehmanBhattii@users.noreply.github.com>
2022-08-31 20:19:16 +0200

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+// if($(window).scrollTop() > Height_of_Header){
+//     //begin to scroll
+//     $(".staticBasket").css("position","fixed");
+//     $(".staticBasket").css("top", 100 , "px");
+// }
+// else{
+//     //lock it back into place
+//     $(".staticBasket").css("position","relative");
+// }
